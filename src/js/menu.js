ae64d85000bf1a741731eb4d7b3ea50f1303b718
Halyna-1983 <72556196+Halyna-1983@users.noreply.github.com>
2021-03-28 16:56:13 +0300

    моя спроба №100

---
+++ src/js/menu.js
@@ -0,0 +1,7 @@
+import menuTemplates from "../templates/menu-items.hbs";
+import menuData from "../menu.json";
+
+const markup = menuTemplates(menuData);
+
+const menuRef = document.querySelector('.js-menu');
+menuRef.insertAdjacentHTML('beforeend', markup)
---
+++ src/js/theme.js
@@ -0,0 +1,39 @@
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+  
+  const changeRef = document.querySelector('#theme-switch-toggle');
+  const bodyRef = document.querySelector('body');
+  
+  if (localStorage.getItem('theme') === 'dark') {
+    applyDark()
+  }
+  else {
+    if (localStorage.getItem('theme') === 'light') {
+      applyLight()
+    }
+  }
+  
+   changeRef.addEventListener("change", changeTheme);
+  
+  function applyDark() {
+    bodyRef.classList.add(Theme.DARK)
+    bodyRef.classList.remove(Theme.LIGHT)
+    changeRef.checked = true
+  }
+  function applyLight() {
+    bodyRef.classList.add(Theme.LIGHT)
+    bodyRef.classList.remove(Theme.DARK)
+    changeRef.checked = false
+  }
+  function changeTheme() {
+    if (changeRef.checked) {
+      applyDark()
+      localStorage.setItem('theme', 'dark')
+    }
+    else {
+      applyLight()
+      localStorage.setItem('theme', 'light')
+    }
+  }
